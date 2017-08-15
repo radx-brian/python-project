addfa07292971b118129c15b44e030045e0a4014
radx-brian <brilar3@gmail.com>
2017-08-15 10:56:52 -0600

    Update story.py
    hey jose i just fixed some grammar issues

--- story.py
+++ story.py
@@ -9,13 +9,13 @@ def begin_story():
 def elevator(user_response, user_name):
     #1. You open the door to the classroom. 
     if (user_response == 1):
-        print("you see what seems like a person run throught the hall")
-        user_response = int(input("1. You say whos there to whatever you saw"))
+        print("you see a person run throught the hall")
+        user_response = int(input("1. You say who is there to whatever you saw"))
         elevator(user_response, user_name)
         
     #2. You attemp to call your mother. 
     elif (user_response == 2):
-        print('You call your  mother she answers but all you hear is breathing then the call ends,', user_name, ', whats going on?.')
+        print('You call your mother she answers but all you hear is breathing then the call ends,', user_name, ', whats going on?.')
         user_response = input("Choices TBD")
 
     #3. You go back to sleep'
